cc21fb19c4e7b418f86b10bd71de1935c049cee4
Msol9 <ms.bksol@gmail.com>
2025-12-04 17:18:09 -0600

    Colocar un switch para minimizar o maximizar segun sea el caso

--- MetodoSimplex.java
+++ MetodoSimplex.java
@@ -2,13 +2,28 @@ import java.util.Scanner;
  class MetodoSimplex {
     public static void main (String [] args){
         Scanner lector= new Scanner (System.in);
+        byte opcion;
+        do{
 
         System.out.println("Metodo simplex:");
         System.out.println("1. Maximizacion");
         System.out.println("2. Minimizacion");
         System.out.println("3. Salir");
-        byte opcion=lector.nextByte();
+        opcion=lector.nextByte();
 
-        
+        switch (opcion){
+            case 1->{
+                System.out.println("Maximizacion");
+            }
+            case 2->{
+                System.out.println("Minimizacion");
+            }
+            default->{
+                if (opcion!=3) {
+                    System.out.println("Opcion invalida, intenta nuevamente.");
+                }
+            }
+        }
+        } while (opcion !=3);
     }
 }
