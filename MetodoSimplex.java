10dfc5b372c7107d929ba6cb9d3eff5c29f05324
Msol9 <ms.bksol@gmail.com>
2025-12-04 17:12:12 -0600

    Creacion del menu para la minimizacion y maximizacion

--- MetodoSimplex.java
+++ MetodoSimplex.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+ class MetodoSimplex {
+    public static void main (String [] args){
+        Scanner lector= new Scanner (System.in);
+
+        System.out.println("Metodo simplex:");
+        System.out.println("1. Maximizacion");
+        System.out.println("2. Minimizacion");
+        System.out.println("3. Salir");
+        byte opcion=lector.nextByte();
 
         
+    }
+}
