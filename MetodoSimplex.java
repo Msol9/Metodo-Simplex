9a506da17f2a96c6a76d6fb932557d1cb3853692
Msol9 <ms.bksol@gmail.com>
2025-12-04 19:16:07 -0600

    Terminar calculos para los resultados de la fila pivote

--- MetodoSimplex.java
+++ MetodoSimplex.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
     public static void main (String [] args){
         Scanner lector= new Scanner (System.in);
         byte opcion;
-        double X1, X2, menorCociente, cociente;
+        double X1, X2, menorCociente, cociente, pivote, multiplicador;
         byte restricciones, fila;
         double [][] tablaSimplex = new double[6][3];
         byte columnaPivote, filaPivote;
@@ -45,17 +45,29 @@ import java.util.Scanner;
                     }else{
                         columnaPivote=1;
                     }
-                }
+                
 
                 menorCociente=Double.MAX_VALUE;
                 for(fila=0;fila<restricciones;fila++){
-                    if (tablaSimplex[fila][columnaPivote]<0) {
+                    if (tablaSimplex[fila][columnaPivote]>0) {
                         cociente = tablaSimplex[fila][2]/tablaSimplex [fila][columnaPivote];
                         if(cociente<menorCociente){
                             menorCociente=cociente;
                             filaPivote=fila;
                         }
                         
+                    }
+                }
+                pivote= tablaSimplex[filaPivote][columnaPivote];
+                for(int i=0; i<3; i++){
+                    tablaSimplex [filaPivote][i]/=pivote;
+                
+                }
+                for(fila=0;fila<=restricciones;fila++){
+                    if(fila!= filaPivote){
+                        multiplicador=tablaSimplex[fila][columnaPivote];
+                        for(int i=0;i<3;i++)
+                            tablaSimplex[fila][j]-=multiplicador*tablaSimplex[filaPivote][i];
                     }
                 }
             }
@@ -83,7 +95,7 @@ import java.util.Scanner;
                 tablaSimplex[restricciones][1]=X2;
                 tablaSimplex[restricciones][2]=0;
 
-                while(tablaSimplex[restricciones][0] < 0 || tablaSimplex[restricciones][1] < 0){
+                while(tablaSimplex[restricciones][0] >0 || tablaSimplex[restricciones][1] > 0){
                     if(tablaSimplex [restricciones][0]< tablaSimplex[restricciones][1]){
                         columnaPivote=0;
                     }else{
